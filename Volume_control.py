1ab7baa9fa7d2527d9b616022443903889afb4ef
Negarnbkhti42 <negar.nobakhti01@gmail.com>
2026-04-02 12:09:50 +0200

    Show fps

--- Volume_control.py
+++ Volume_control.py
@@ -3,11 +3,19 @@ import numpy as np
 import time
 
 cap = cv2.VideoCapture(0)
+pastTime = 0
 
 while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
     img = cv2.resize(img, (640, 480))
+
+    currentTime = time.time()
+    fps = 1 / (currentTime - pastTime)
+    pastTime = currentTime
+    cv2.putText(
+        img, f"FPS: {int(fps)}", (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (21, 171, 91), 1
+    )
 
     cv2.imshow("Img", img)
     if cv2.waitKey(1) & 0xFF == ord("q"):
